55e1c64d5dd466c27743a9d5727db089199b1a13
Tuomas <me@tuomas.id>
2026-02-05 10:02:41 +0200

    Making everything play well with Deno + update Supabase functions

--- supabase/functions/hello/index.ts
+++ supabase/functions/hello/index.ts
@@ -2,15 +2,15 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-import { serve } from "https://deno.land/std@0.177.1/http/server.ts"
+// deno-lint-ignore-file no-import-prefix
+import { serve } from 'https://deno.land/std@0.177.1/http/server.ts';
 
 serve(async () => {
-  return new Response(
-    `"Hello from Edge Functions!"`,
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
+  return new Response(`"Hello from Edge Functions!"`, {
+    headers: { 'Content-Type': 'application/json' },
+  });
+});
 
 // To invoke:
-// curl 'http://localhost:<KONG_HTTP_PORT>/functions/v1/hello' \
+// curl '<Your Supabase component URL>/functions/v1/hello' \
 //   --header 'Authorization: Bearer <anon/service_role API key>'
--- supabase/functions/main/index.ts
+++ supabase/functions/main/index.ts
@@ -1,94 +1,340 @@
-import { serve } from 'https://deno.land/std@0.131.0/http/server.ts'
-import * as jose from 'https://deno.land/x/jose@v4.14.4/index.ts'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// deno-lint-ignore-file no-import-prefix no-explicit-any
 
-console.log('main function started')
+/**
+ * This is the Supabase Edge Functions entrypoint.
+ *
+ * It bootstraps the Edge Runtime and routes incoming HTTP requests
+ * to the appropriate function inside the /functions directory.
+ *
+ * It is required when running Edge Functions in a self-hosted setup,
+ * but does not contain application business logic.
+ *
+ * You normally do not need to modify this file.
+ *
+ * @see https://github.com/supabase/cli/blob/main/internal/functions/serve/templates/main.ts
+ */
 
-const JWT_SECRET = Deno.env.get('JWT_SECRET')
-const VERIFY_JWT = Deno.env.get('VERIFY_JWT') === 'true'
+// Setup type definitions for built-in Supabase Runtime APIs
+import 'jsr:@supabase/functions-js@2.94.1/edge-runtime.d.ts';
 
-function getAuthToken(req: Request) {
-  const authHeader = req.headers.get('authorization')
-  if (!authHeader) {
-    throw new Error('Missing authorization header')
-  }
-  const [bearer, token] = authHeader.split(' ')
-  if (bearer !== 'Bearer') {
-    throw new Error(`Auth header is not 'Bearer {token}'`)
-  }
-  return token
+import { STATUS_CODE, STATUS_TEXT } from 'https://deno.land/std/http/status.ts';
+import * as posix from 'https://deno.land/std/path/posix/mod.ts';
+import * as jose from 'https://deno.land/x/jose@v4.13.1/index.ts';
+
+const SB_SPECIFIC_ERROR_CODE = {
+  BootError:
+    STATUS_CODE.ServiceUnavailable /** Service Unavailable (RFC 7231, 6.6.4) */,
+  InvalidWorkerResponse:
+    STATUS_CODE.InternalServerError /** Internal Server Error (RFC 7231, 6.6.1) */,
+  WorkerLimit: 546 /** Extended */,
+};
+
+const SB_SPECIFIC_ERROR_TEXT = {
+  [SB_SPECIFIC_ERROR_CODE.BootError]: 'BOOT_ERROR',
+  [SB_SPECIFIC_ERROR_CODE.InvalidWorkerResponse]: 'WORKER_ERROR',
+  [SB_SPECIFIC_ERROR_CODE.WorkerLimit]: 'WORKER_LIMIT',
+};
+
+const SB_SPECIFIC_ERROR_REASON = {
+  [SB_SPECIFIC_ERROR_CODE.BootError]:
+    'Worker failed to boot (please check logs)',
+  [SB_SPECIFIC_ERROR_CODE.InvalidWorkerResponse]:
+    'Function exited due to an error (please check logs)',
+  [SB_SPECIFIC_ERROR_CODE.WorkerLimit]:
+    'Worker failed to respond due to a resource limit (please check logs)',
+};
+
+// OS stuff - we don't want to expose these to the functions.
+const EXCLUDED_ENVS = ['HOME', 'HOSTNAME', 'PATH', 'PWD'];
+
+const JWT_SECRET = Deno.env.get('SUPABASE_INTERNAL_JWT_SECRET')!;
+const HOST_PORT = Deno.env.get('SUPABASE_INTERNAL_HOST_PORT')!;
+const DEBUG = Deno.env.get('SUPABASE_INTERNAL_DEBUG') === 'true';
+const FUNCTIONS_CONFIG_STRING = Deno.env.get(
+  'SUPABASE_INTERNAL_FUNCTIONS_CONFIG',
+)!;
+
+const WALLCLOCK_LIMIT_SEC = parseInt(
+  Deno.env.get('SUPABASE_INTERNAL_WALLCLOCK_LIMIT_SEC') as string,
+);
+
+const DENO_SB_ERROR_MAP = new Map([
+  [
+    (Deno.errors as any).InvalidWorkerCreation,
+    SB_SPECIFIC_ERROR_CODE.BootError,
+  ],
+  [
+    (Deno.errors as any).InvalidWorkerResponse,
+    SB_SPECIFIC_ERROR_CODE.InvalidWorkerResponse,
+  ],
+  [Deno.errors.WorkerRequestCancelled, SB_SPECIFIC_ERROR_CODE.WorkerLimit],
+]);
+const GENERIC_FUNCTION_SERVE_MESSAGE = `Serving functions on http://127.0.0.1:${HOST_PORT}/functions/v1/<function-name>`;
+
+interface FunctionConfig {
+  entrypointPath: string;
+  importMapPath: string;
+  verifyJWT: boolean;
+  staticFiles?: string[];
 }
 
-async function verifyJWT(jwt: string): Promise<boolean> {
-  const encoder = new TextEncoder()
-  const secretKey = encoder.encode(JWT_SECRET)
-  try {
-    await jose.jwtVerify(jwt, secretKey)
-  } catch (err) {
-    console.error(err)
-    return false
-  }
-  return true
-}
+function getResponse(payload: any, status: number, customHeaders = {}) {
+  const headers: { [key: string]: string } = { ...customHeaders };
+  let body: string | null = null;
 
-serve(async (req: Request) => {
-  if (req.method !== 'OPTIONS' && VERIFY_JWT) {
-    try {
-      const token = getAuthToken(req)
-      const isValidJWT = await verifyJWT(token)
-
-      if (!isValidJWT) {
-        return new Response(JSON.stringify({ msg: 'Invalid JWT' }), {
-          status: 401,
-          headers: { 'Content-Type': 'application/json' },
-        })
-      }
-    } catch (e) {
-      console.error(e)
-      return new Response(JSON.stringify({ msg: e.toString() }), {
-        status: 401,
-        headers: { 'Content-Type': 'application/json' },
-      })
+  if (payload) {
+    if (typeof payload === 'object') {
+      headers['Content-Type'] = 'application/json';
+      body = JSON.stringify(payload);
+    } else if (typeof payload === 'string') {
+      headers['Content-Type'] = 'text/plain';
+      body = payload;
+    } else {
+      body = null;
     }
   }
 
-  const url = new URL(req.url)
-  const { pathname } = url
-  const path_parts = pathname.split('/')
-  const service_name = path_parts[1]
+  return new Response(body, { status, headers });
+}
 
-  if (!service_name || service_name === '') {
-    const error = { msg: 'missing function name in request' }
-    return new Response(JSON.stringify(error), {
-      status: 400,
-      headers: { 'Content-Type': 'application/json' },
-    })
-  }
-
-  const servicePath = `/home/deno/functions/${service_name}`
-  console.error(`serving the request with ${servicePath}`)
-
-  const memoryLimitMb = 150
-  const workerTimeoutMs = 1 * 60 * 1000
-  const noModuleCache = false
-  const importMapPath = null
-  const envVarsObj = Deno.env.toObject()
-  const envVars = Object.keys(envVarsObj).map((k) => [k, envVarsObj[k]])
-
+const functionsConfig: Record<string, FunctionConfig> = (() => {
   try {
-    const worker = await EdgeRuntime.userWorkers.create({
-      servicePath,
-      memoryLimitMb,
-      workerTimeoutMs,
-      noModuleCache,
-      importMapPath,
-      envVars,
-    })
-    return await worker.fetch(req)
-  } catch (e) {
-    const error = { msg: e.toString() }
-    return new Response(JSON.stringify(error), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    })
+    const functionsConfig = JSON.parse(FUNCTIONS_CONFIG_STRING);
+
+    if (DEBUG) {
+      console.log(
+        'Functions config:',
+        JSON.stringify(functionsConfig, null, 2),
+      );
+    }
+
+    return functionsConfig;
+  } catch (cause) {
+    throw new Error('Failed to parse functions config', { cause });
   }
-})
+})();
+
+function getAuthToken(req: Request) {
+  const authHeader = req.headers.get('authorization');
+  if (!authHeader) {
+    throw new Error('Missing authorization header');
+  }
+  const [bearer, token] = authHeader.split(' ');
+  if (bearer !== 'Bearer') {
+    throw new Error(`Auth header is not 'Bearer {token}'`);
+  }
+  return token;
+}
+
+async function verifyJWT(jwt: string): Promise<boolean> {
+  const encoder = new TextEncoder();
+  const secretKey = encoder.encode(JWT_SECRET);
+  try {
+    await jose.jwtVerify(jwt, secretKey);
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+  return true;
+}
+
+// Ref: https://docs.deno.com/examples/checking_file_existence/
+async function shouldUsePackageJsonDiscovery({
+  entrypointPath,
+  importMapPath,
+}: FunctionConfig): Promise<boolean> {
+  if (importMapPath) {
+    return false;
+  }
+  const packageJsonPath = posix.join(
+    posix.dirname(entrypointPath),
+    'package.json',
+  );
+  try {
+    await Deno.lstat(packageJsonPath);
+  } catch (err) {
+    if (err instanceof Deno.errors.NotFound) {
+      return false;
+    }
+  }
+  return true;
+}
+
+Deno.serve({
+  handler: async (req: Request) => {
+    const url = new URL(req.url);
+    const { pathname } = url;
+
+    // handle health checks
+    if (pathname === '/_internal/health') {
+      return getResponse({ message: 'ok' }, STATUS_CODE.OK);
+    }
+
+    // handle metrics
+    if (pathname === '/_internal/metric') {
+      const metric = await EdgeRuntime.getRuntimeMetrics();
+      return Response.json(metric);
+    }
+
+    const pathParts = pathname.split('/');
+    const functionName = pathParts[1];
+
+    if (!functionName || !(functionName in functionsConfig)) {
+      return getResponse('Function not found', STATUS_CODE.NotFound);
+    }
+
+    if (req.method !== 'OPTIONS' && functionsConfig[functionName].verifyJWT) {
+      try {
+        const token = getAuthToken(req);
+        const isValidJWT = await verifyJWT(token);
+
+        if (!isValidJWT) {
+          return getResponse({ msg: 'Invalid JWT' }, STATUS_CODE.Unauthorized);
+        }
+      } catch (e: any) {
+        console.error(e);
+        return getResponse({ msg: e.toString() }, STATUS_CODE.Unauthorized);
+      }
+    }
+
+    const servicePath = posix.dirname(
+      functionsConfig[functionName].entrypointPath,
+    );
+    console.error(`serving the request with ${servicePath}`);
+
+    // Ref: https://supabase.com/docs/guides/functions/limits
+    const memoryLimitMb = 256;
+    const workerTimeoutMs = isFinite(WALLCLOCK_LIMIT_SEC)
+      ? WALLCLOCK_LIMIT_SEC * 1000
+      : 400 * 1000;
+    const noModuleCache = false;
+    const envVarsObj = Deno.env.toObject();
+    const envVars = Object.entries(envVarsObj).filter(
+      ([name, _]) =>
+        !EXCLUDED_ENVS.includes(name) && !name.startsWith('SUPABASE_INTERNAL_'),
+    );
+
+    const forceCreate = false;
+    const customModuleRoot = ''; // empty string to allow any local path
+    const cpuTimeSoftLimitMs = 1000;
+    const cpuTimeHardLimitMs = 2000;
+
+    // NOTE(Nyannyacha): Decorator type has been set to tc39 by Lakshan's request,
+    // but in my opinion, we should probably expose this to customers at some
+    // point, as their migration process will not be easy.
+    // This need to be kept for Deno 1 compatibility.
+    const decoratorType = 'tc39';
+
+    const absEntrypoint = posix.join(
+      Deno.cwd(),
+      functionsConfig[functionName].entrypointPath,
+    );
+    const maybeEntrypoint = posix.toFileUrl(absEntrypoint).href;
+    const usePackageJson = await shouldUsePackageJsonDiscovery(
+      functionsConfig[functionName],
+    );
+
+    const staticPatterns = functionsConfig[functionName].staticFiles;
+
+    try {
+      const worker = await EdgeRuntime.userWorkers.create({
+        servicePath,
+        memoryLimitMb,
+        workerTimeoutMs,
+        noModuleCache,
+        // @ts-expect-error Supabase edge runtime supports noNpm
+        noNpm: !usePackageJson,
+        importMapPath: functionsConfig[functionName].importMapPath,
+        envVars,
+        forceCreate,
+        customModuleRoot,
+        cpuTimeSoftLimitMs,
+        cpuTimeHardLimitMs,
+        decoratorType,
+        maybeEntrypoint,
+        context: {
+          useReadSyncFileAPI: true,
+        },
+        staticPatterns,
+      });
+
+      return await worker.fetch(req);
+    } catch (e: any) {
+      console.error(e);
+
+      for (const [denoError, sbCode] of DENO_SB_ERROR_MAP.entries()) {
+        if (denoError !== void 0 && e instanceof denoError) {
+          return getResponse(
+            {
+              code: SB_SPECIFIC_ERROR_TEXT[sbCode],
+              message: SB_SPECIFIC_ERROR_REASON[sbCode],
+            },
+            sbCode,
+          );
+        }
+      }
+
+      return getResponse(
+        {
+          code: STATUS_TEXT[STATUS_CODE.InternalServerError],
+          message: 'Request failed due to an internal server error',
+          trace: JSON.stringify(e.stack),
+        },
+        STATUS_CODE.InternalServerError,
+      );
+    }
+  },
+
+  onListen: () => {
+    try {
+      const functionsConfigString = Deno.env.get(
+        'SUPABASE_INTERNAL_FUNCTIONS_CONFIG',
+      );
+      if (functionsConfigString) {
+        const MAX_FUNCTIONS_URL_EXAMPLES = 5;
+        const functionsConfig = JSON.parse(functionsConfigString) as Record<
+          string,
+          unknown
+        >;
+        const functionNames = Object.keys(functionsConfig);
+        const exampleFunctions = functionNames.slice(
+          0,
+          MAX_FUNCTIONS_URL_EXAMPLES,
+        );
+        const functionsUrls = exampleFunctions.map(
+          (fname) => ` - http://127.0.0.1:${HOST_PORT}/functions/v1/${fname}`,
+        );
+        const functionsExamplesMessages =
+          functionNames.length > 0
+            ? // Show some functions urls examples
+              `\n${functionsUrls.join(`\n`)}${
+                functionNames.length > MAX_FUNCTIONS_URL_EXAMPLES
+                  ? // If we have more than 10 functions to serve, then show examples for first 10
+                    // and a count for the remaining ones
+                    `\n... and ${functionNames.length - MAX_FUNCTIONS_URL_EXAMPLES} more functions`
+                  : ''
+              }`
+            : '';
+        console.log(
+          `${GENERIC_FUNCTION_SERVE_MESSAGE}${functionsExamplesMessages}\nUsing ${Deno.version.deno}`,
+        );
+      }
+    } catch (e) {
+      console.log(
+        `${GENERIC_FUNCTION_SERVE_MESSAGE}\nUsing ${Deno.version.deno}`,
+      );
+    }
+  },
+
+  onError: (e: any) => {
+    return getResponse(
+      {
+        code: STATUS_TEXT[STATUS_CODE.InternalServerError],
+        message: 'Request failed due to an internal server error',
+        trace: JSON.stringify(e.stack),
+      },
+      STATUS_CODE.InternalServerError,
+    );
+  },
+});
